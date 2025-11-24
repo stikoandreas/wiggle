59ec5fd2945c41afa723f2b0cf4b54d451329c65
Andreas Persson <stikoandreas@gmail.com>
2025-11-24 23:16:45 +0100

    Added zoom support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import '@mantine/core/styles.css';
 import '@mantine/dropzone/styles.css';
 import 'react-image-crop/dist/ReactCrop.css';
-import '@gfazioli/mantine-split-pane/styles.css';
 
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
--- src/components/CoordSelector/CoordSelector.tsx
+++ src/components/CoordSelector/CoordSelector.tsx
@@ -1,4 +1,5 @@
-import { memo, useMemo, useState } from 'react';
+import { memo, useMemo, useRef, useState } from 'react';
+import PrismaZoom from 'react-prismazoom';
 import { AspectRatio, Center, Group, Image, UnstyledButton } from '@mantine/core';
 import { renderImage, renderThumbnail } from '@/components/ImageInput/ImageInput';
 
@@ -24,7 +25,7 @@ export const CoordSelectorGrid = memo(
         </AspectRatio>
         <Group justify="center" mt="xs">
           {images.map((_, index) => (
-            <UnstyledButton key={index}>
+            <UnstyledButton key={index} style={{ zIndex: 1000 }}>
               <Image
                 src={
                   coords[index].x && coords[index].y
@@ -63,60 +64,75 @@ function CoordSelector({
 }) {
   // Click on image to select image pixel coordinates, based on the image itself
 
+  const mouseCoords = useRef<{ x: number; y: number } | null>(null);
+
   const imageSrc = useMemo(() => renderImage(image), [image]);
 
   const invertedCursor = true;
 
   return (
     <Center mah="80dvh">
-      <div style={{ position: 'relative', display: 'inline-block' }}>
-        <Image
-          src={imageSrc}
+      <PrismaZoom allowTouchEvents>
+        <UnstyledButton
+          style={{ position: 'relative', display: 'inline-block' }}
+          onMouseDown={(e) => (mouseCoords.current = { x: e.clientX, y: e.clientY })}
           onClick={(e) => {
+            if (mouseCoords.current) {
+              const deltaX = e.clientX - mouseCoords.current.x;
+              const deltaY = e.clientY - mouseCoords.current.y;
+              if (Math.sqrt(deltaX * deltaX + deltaY * deltaY) > 5) {
+                return;
+              }
+            }
             const rect = (e.target as HTMLImageElement).getBoundingClientRect();
             const x = ((e.clientX - rect.left) / rect.width) * image.width;
             const y = ((e.clientY - rect.top) / rect.height) * image.height;
             onSelect({ x, y, w: image.width, h: image.height });
+            mouseCoords.current = null;
           }}
-          mah="80dvh"
-          style={{
-            //cursor: `url(${renderCursor(image)}) 20 20, pointer`,
-            cursor: 'crosshair',
-          }}
-        />
-        {coords.x > 0 &&
-          coords.y > 0 &&
-          (invertedCursor ? (
-            <Image
-              src={imageSrc}
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-              style={{
-                filter: 'invert(1)',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                pointerEvents: 'none',
-                maskImage: `radial-gradient(circle 4px at ${(coords.x / coords.w) * 100}%  ${(coords.y / coords.h) * 100}%, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 80%, rgba(0,0,0,0) 100%)`,
-              }}
-            />
-          ) : (
-            <div
-              style={{
-                position: 'absolute',
-                left: `calc(${(coords.x / coords.w) * 100}%)`,
-                top: `calc(${(coords.y / coords.h) * 100}%)`,
-                width: '8px',
-                height: '8px',
-                backgroundColor: 'red',
-                borderRadius: '50%',
-                transform: 'translate(-50%, -50%)',
-                pointerEvents: 'none',
-              }}
-            />
-          ))}
-      </div>
+        >
+          <Image
+            src={imageSrc}
+            mah="80dvh"
+            style={{
+              //cursor: `url(${renderCursor(image)}) 20 20, pointer`,
+              cursor: 'crosshair',
+            }}
+          />
+          {coords.x > 0 &&
+            coords.y > 0 &&
+            (invertedCursor ? (
+              <Image
+                src={imageSrc}
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                style={{
+                  filter: 'invert(1)',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  pointerEvents: 'none',
+                  maskImage: `radial-gradient(circle 4px at ${(coords.x / coords.w) * 100}%  ${(coords.y / coords.h) * 100}%, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 80%, rgba(0,0,0,0) 100%)`,
+                }}
+              />
+            ) : (
+              <div
+                style={{
+                  position: 'absolute',
+                  left: `calc(${(coords.x / coords.w) * 100}%)`,
+                  top: `calc(${(coords.y / coords.h) * 100}%)`,
+                  width: '8px',
+                  height: '8px',
+                  backgroundColor: 'red',
+                  borderRadius: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  pointerEvents: 'none',
+                }}
+              />
+            ))}
+        </UnstyledButton>
+      </PrismaZoom>
     </Center>
   );
 }
